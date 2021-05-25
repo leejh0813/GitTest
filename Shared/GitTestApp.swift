c6ca5b2e4c80693dd56d5bfb02ca2d85e0cd089a
이재홍 <leejh0813@kakao.com>
2021-05-25 20:45:34 +0900

    Initial Commit

---
+++ Shared/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  Shared
+//
+//  Created by 이재홍 on 2021/05/25.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
